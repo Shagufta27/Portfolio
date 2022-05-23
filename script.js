f3a217c74b5210951cddf1658de568363cdd8a95
Shagufta Yasmin <shaggykat27@gmail.com>
2022-05-23 23:09:52 +0600

    Worked on the contact page and the footer section. The Website is complete!

--- script.js
+++ script.js
@@ -3,9 +3,35 @@ $(document).ready(function(){
 		if(this.scrollY > 20){
 			$('.navbar').addClass("sticky");
 		}else{
-			$('navbar').removeClass("sticky");
+			$('.navbar').removeClass("sticky");
+		}
+		if(this.scrollY > 500){
+			$('.scoll-up-btn').addClass("show");
+		}else{
+			$('.scoll-up-btn').removeClass("show");
 		}
 	});
+
+	//slide-up script
+	$('scroll-up-btn').click(function(){
+		$('html').animated({scrollTop: 0});
+	});
+
+	//typing animation script
+	var typed = new Typed(".typing", {
+		strings: ["Developer", "Designer", "Content Writer", "Freelancer"],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop: true
+	});
+
+	var typed = new Typed(".typing-2", {
+		strings: ["Developer", "Designer", "Content Writer", "Freelancer"],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop: true
+	});
+
 
 	//toggle menu/navbar script
 	$('.menu-btn').click(function(){
